791f61833d2071d4488568ee3ab1620e864446ec
abuchanan1 <aaronbuch10@gmail.com>
2023-03-10 17:50:34 +0000

    created partials for the nav bar and cdn assests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'properties#index'
+  
+  devise_for :users
+  
   resources :comments
   resources :properties
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
